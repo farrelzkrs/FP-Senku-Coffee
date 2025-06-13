0757719e1c152602aac9a64f1c4621d7f11bf07c
Farrel Zikri <69224844+farrelzkrs@users.noreply.github.com>
2025-06-14 01:49:48 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -85,6 +85,8 @@ $conn->query("CREATE TABLE IF NOT EXISTS feedback (
   tanggal_kirim DATETIME DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (user_id) REFERENCES users(id_users)
 )");
+
+// Tabel admin (untuk save admin dan password)
 $conn->query("CREATE TABLE IF NOT EXISTS admins (
   id_admins INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(100) NOT NULL UNIQUE,
