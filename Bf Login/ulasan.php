b3f7962dee12f8415d04b9c648ed2704cbedbf21
Muhammad Naufal Ardiansyah <143949563+NaufalSyh@users.noreply.github.com>
2025-06-13 18:35:24 +0700

    Upload baru

---
+++ Bf Login/ulasan.php
@@ -0,0 +1,37 @@
+<?php
+include 'auth.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous">
+        </script>
+    <link rel="stylesheet" href="StyleCSS/ulasanstyle.css">
+    <title>Ulasan - Senku Coffee</title>
+</head>
+
+<body>
+    <div class="header">
+        <!-- header -->
+        <?php include 'navbarbf.php'; ?>
+    </div>
+    <div class="penutup">
+        <footer class="text-center mb-2 mt-3">
+            <hr>
+            <small>
+                &copy; 2025 Senku Coffee &middot;
+                Jl. Kopi No. 123, Jakarta &middot;
+                <a href="mailto:info@senkucoffee.com" class="text-decoration-none">info@senkucoffee.com</a>
+            </small>
+        </footer>
+    </div>
+</body>
+
+</html>
