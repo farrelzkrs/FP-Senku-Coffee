3a647469c5015b72363b537e5f19e4e9f84ca7a6
Muhammad Naufal Ardiansyah <143949563+NaufalSyh@users.noreply.github.com>
2025-06-13 18:34:17 +0700

    delall

--- Bf Login/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-// filepath: c:\laragon\www\FP Pemweb\Bf Login\auth.php
-session_start();
-if (!isset($_SESSION['user_id'])) {
-    // Redirect ke halaman login jika belum login
-    header("Location: masuk.php");
-    exit;
-}
